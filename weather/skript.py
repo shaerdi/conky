2e44207007a3ae60e8da43efa3602a5fcd451522
Simon Haerdi <shaerdi@gmail.com>
2013-11-06 16:31:46 +0100

    initial commit

---
+++ weather/skript.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from lxml import etree
+import time,os,sys,shutil
+
+url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Chur&cnt=3&mode=xml&units=metric'
+
+parsed = etree.parse(url)
+
+loc = parsed.find('location').getchildren()[0].text
+print loc
+
+fc = parsed.find('forecast').getchildren()
+
+num = len(fc)
+
+dates = [d.values()[0] for d in fc]
+days = [time.strftime('%a',time.strptime(t,'%Y-%m-%d')) for t in dates]
+
+syms = [f[0].attrib['var'] for f in fc]
+names = [f[0].attrib['name'] for f in fc]
+
+tempsMax = [float(f[4].attrib['max']) for f in fc]
+tempsMin = [float(f[4].attrib['min']) for f in fc]
+
+tempsMax = ['{: >6.2f} C'.format(t) for t in tempsMax]
+tempsMin = ['{: >6.2f} C'.format(t) for t in tempsMin]
+
+pathname = os.path.dirname(sys.argv[0])
+path = os.path.abspath(pathname)
+
+for i,pic in enumerate(syms):
+    try:
+        shutil.copy(path+'/pics/'+pic+'.png', path+'/pics/current/' + str(i))
+    except IOError:
+        shutil.copy(path+'/pics/err.png', path+'/pics/current' + str(i))
+
+
+with open(path+'/data/testout','w') as myFile:
+    for d in zip(days,names,tempsMin,tempsMax):
+        myFile.write(','.join(d))
+        myFile.write('\n')
+
+#print 'test'
+#print ','.join(map(str,tempsMax))
